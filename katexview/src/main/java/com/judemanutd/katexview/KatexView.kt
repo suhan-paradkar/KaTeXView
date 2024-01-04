c08be1a3f350c4df6d57634a4ad952e01c614be7
Wisest_wizard <12suhangp34@gmail.com>
2024-01-04 10:00:38 +0530

    Modernize to kotlin and migrate to compose

--- katexview/src/main/java/com/judemanutd/katexview/KatexView.kt
+++ katexview/src/main/java/com/judemanutd/katexview/KatexView.kt
@@ -2,93 +2,89 @@ package com.judemanutd.katexview
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Color
-import android.util.AttributeSet
 import android.webkit.WebSettings
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
 import com.x5.template.Chunk
 import com.x5.template.Theme
 import com.x5.template.providers.AndroidTemplates
-import androidx.core.content.ContextCompat
 
+
+private const val TAG_FORMULA: String = "formula"
+private const val TAG_TEXT_COLOR: String = "textColor"
+private const val TAG_TEXT_SIZE: String = "textSize"
+
+private var mText: String? = null
+//TODO: pick theme set text color
+private var mTextColor: Int = android.R.color.holo_purple
+private var mTextSize: Int = 20
 @SuppressLint("SetJavaScriptEnabled")
-class KatexView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) :
-    WebView(context, attrs, defStyleAttr) {
+@Composable
+fun KatexView(text: String, textColor: Int, backgroundColor: Int, textSize: Int) {
+    AndroidView(factory = {
 
-    private val TAG_FORMULA: String = "formula"
-    private val TAG_TEXT_COLOR: String = "textColor"
-
-    private var mText: String? = null
-    //TODO: pick theme set text color
-    private var mTextColor: Int = android.R.color.holo_purple
-
-    init {
-        settings.allowFileAccess = true
-        settings.javaScriptEnabled = true
-        settings.cacheMode = WebSettings.LOAD_NO_CACHE
-        settings.displayZoomControls = false
-        settings.builtInZoomControls = false
-        settings.setSupportZoom(false)
-        settings.useWideViewPort = false
-
-        setBackgroundColor(Color.TRANSPARENT);
-
-        val mTypeArray = context.obtainStyledAttributes(attrs, R.styleable.KatexView, 0, 0)
-
-        try {
-            setTextColor(
-                mTypeArray.getColor(
-                    R.styleable.KatexView_textColor,
-                    ContextCompat.getColor(getContext(), android.R.color.black)
-                )
-            )
-
-            val text: String? = mTypeArray.getString(R.styleable.KatexView_text)
-            if (text != null)
-                setText(text)
-        } finally {
-            mTypeArray.recycle()
+        WebView(it).apply {
+            settings.allowFileAccess = true
+            settings.allowFileAccessFromFileURLs = true
+            settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+            settings.allowUniversalAccessFromFileURLs = true
+            settings.javaScriptEnabled = true
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            settings.displayZoomControls = false
+            settings.builtInZoomControls = false
+            settings.setSupportZoom(false)
+            settings.useWideViewPort = false
+            setBackgroundColor(backgroundColor)
+            setText(text, it)
+            setTextColor(textColor, it)
+            setTextSize(textSize, it)
+            loadDataWithBaseURL(null, loadData(it), "text/html", "utf-8", "about:blank")
+            isVerticalScrollBarEnabled = false
+            isHorizontalScrollBarEnabled = false
+            webViewClient = WebViewClient()
         }
+    })
+}
 
+fun setText(text: String, context: Context) {
+    mText = text
+    loadData(context)
+}
+
+fun setTextColor(color: Int, context: Context) {
+    mTextColor = color
+    loadData(context)
+}
+
+fun setTextSize(size: Int, context: Context) {
+    mTextSize = size
+    loadData(context)
+}
+
+fun getText(): String? {
+    return mText
+}
+
+private fun loadData(context: Context): String {
+    if (mText != null) {
+        val chunk: Chunk = getChunk(context = context)
+        chunk.set(TAG_FORMULA, mText)
+        chunk.set(TAG_TEXT_COLOR, getHexColor(mTextColor))
+        chunk.set(TAG_TEXT_SIZE, mTextSize)
+        return chunk.toString()
     }
+    return ""
+}
 
-    fun setText(text: String) {
-        mText = text
-        loadData()
-    }
+private fun getChunk(context: Context): Chunk {
+    val loader = AndroidTemplates(context)
+    return Theme(loader).makeChunk("katex")
+}
 
-    fun setTextColor(color: Int) {
-        this.mTextColor = color
-        loadData()
-    }
-
-    fun getText(): String? {
-        return mText
-    }
-
-    private fun loadData() {
-        if (mText != null) {
-            val chunk: Chunk = getChunk()
-            chunk.set(TAG_FORMULA, mText)
-            chunk.set(TAG_TEXT_COLOR, getHexColor(mTextColor))
-            this.loadDataWithBaseURL(null, chunk.toString(), "text/html", "utf-8", "about:blank")
-        }
-
-    }
-
-    private fun getChunk(): Chunk {
-        val loader = AndroidTemplates(context)
-        return Theme(loader).makeChunk("katex")
-    }
-
-    private fun getHexColor(intColor: Int): String {
-        //Android and javascript color format differ javascript support Hex color,
-        // so the android color which user sets is converted to hexcolor to replicate the same in javascript.
-        return String.format("#%06X", 0xFFFFFF and intColor)
-    }
-
+private fun getHexColor(intColor: Int): String {
+    //Android and javascript color format differ javascript support Hex color,
+    // so the android color which user sets is converted to hexcolor to replicate the same in javascript.
+    return String.format("#%06X", 0xFFFFFF and intColor)
 }
